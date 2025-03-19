e8a7d2143c6f9389100b0499ed5405619297ee91
anastasia_cher <anastasiia.cher@yandex.ru>
2025-03-19 10:08:03 +0300

    Update and rename test_hackernews.py to test_habrnews.py

--- homework06/tests/test_habrnews.py
+++ homework06/tests/test_habrnews.py
@@ -4,19 +4,18 @@ from unittest.mock import call
 
 from boddle import boddle
 from db import News
-from hackernews import add_label, classify_news, update_news
+from habrnews import add_label, classify_news, update_news
 
 
-class TestHackernews(unittest.TestCase):
-    @mock.patch("hackernews.session")
+class TestHabrnews(unittest.TestCase):
+    @mock.patch("habrnews.session")
     def test_add_label(self, session):
         with boddle(query={"id": 1, "label": "never"}):
             news = News(
                 title="Ask HN: Do you use an optimization solver? Which one? Do you like it?",
                 author="ryan-nextmv",
                 url="https://news.ycombinator.com/item?id=31099186",
-                comments=83,
-                points=171,
+                complexity="Средний",
                 label=None,
             )
             session.return_value.query.return_value.get.return_value = news
@@ -24,28 +23,25 @@ class TestHackernews(unittest.TestCase):
             self.assertTrue(news.label == "never")
             self.assertTrue(session.mock_calls[-1] == call().commit())
 
-    @mock.patch("hackernews.get_news")
-    @mock.patch("hackernews.session")
+    @mock.patch("habrnews.get_news")
+    @mock.patch("habrnews.session")
     def test_update_news(self, session, get_news):
         news = [
             {
                 "author": "ngaut",
-                "comments": 24,
-                "points": 97,
+                "complexity": "Средний",
                 "title": "Go will use pdqsort in next release",
                 "url": "https://github.com/golang/go/commit/72e77a7f41bbf45d466119444307fd3ae996e257",
             },
             {
                 "author": "electric_muse",
-                "comments": 44,
-                "points": 85,
+                "complexity": "Средний",
                 "title": "Show HN: Two-way Jira sync in a collaborative spreadsheet and Gantt",
                 "url": "https://www.visor.us/landing/visor-for-jira-launch",
             },
             {
                 "author": "ryan-nextmv",
-                "comments": 83,
-                "points": 171,
+                "complexity": "Средний",
                 "title": "Ask HN: Do you use an optimization solver? Which one? Do you like it?",
                 "url": "https://news.ycombinator.com/item?id=31099186",
             },
@@ -63,47 +59,42 @@ class TestHackernews(unittest.TestCase):
                 n_commit += 1
         self.assertEqual(2, n_commit)
 
-    @mock.patch("hackernews.session")
+    @mock.patch("habrnews.session")
     def test_classify_news(self, session):
         news_cl = [
             News(
                 title="Just a random example",
                 author="ngaut",
                 url="https://github.com/golang/go/commit/72e77a7f41bbf45d466119444307fd3ae996e257",
-                comments=24,
-                points=97,
+                complexity="Средний",
                 label="good",
             ),
             News(
                 title="Another name appeared on the wall",
                 author="electric_muse",
                 url="https://www.visor.us/landing/visor-for-jira-launch",
-                comments=44,
-                points=85,
+                complexity="Средний",
                 label="maybe",
             ),
             News(
                 title="Getting started with Python is easy",
                 author="ryan-nextmv",
                 url="https://news.ycombinator.com/item?id=31099186",
-                comments=83,
-                points=171,
+                complexity="Средний",
                 label="never",
             ),
             News(
                 title="Getting started with Java is easy",
                 author="ryan-nextmv",
                 url="https://news.ycombinator.com/item?id=31099186",
-                comments=83,
-                points=171,
+                complexity="Средний",
                 label="never",
             ),
             News(
                 title="One more random example",
                 author="ryan-nextmv",
                 url="https://news.ycombinator.com/item?id=31099186",
-                comments=83,
-                points=171,
+                complexity="Средний",
                 label="good",
             ),
         ]
@@ -112,22 +103,19 @@ class TestHackernews(unittest.TestCase):
                 title="A name suddenly appeared on the wall",
                 author="ngaut",
                 url="https://github.com/golang/go/commit/72e77a7f41bbf45d466119444307fd3ae996e257",
-                comments=24,
-                points=97,
+                complexity="Простой",
             ),
             News(
                 title="Getting started with C++ is relatively hard",
                 author="electric_muse",
                 url="https://www.visor.us/landing/visor-for-jira-launch",
-                comments=44,
-                points=85,
+                complexity="Простой",
             ),
             News(
                 title="Just another random example",
                 author="ryan-nextmv",
                 url="https://news.ycombinator.com/item?id=31099186",
-                comments=83,
-                points=171,
+                complexity="Простой",
             ),
         ]
         session.return_value.query.return_value.filter.return_value.all.side_effect = [
